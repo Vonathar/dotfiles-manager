069b24500cf5f0d8fe050332d6497c6247f29918
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2021-01-04 15:53:15 +0000

    Add tests for StringFormatter

---
+++ src/test/java/uk/caputo/dotfilesmanager/StringFormatterTest.java
@@ -0,0 +1,27 @@
+package uk.caputo.dotfilesmanager;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringFormatterTest {
+
+  @Autowired StringFormatter stringFormatter;
+
+  @Test
+  void addLeftPadding_ShouldAddLeftPadding() {
+    assertEquals(stringFormatter.addLeftPadding("X", 1), "X");
+    assertEquals(stringFormatter.addLeftPadding("X", 3), "  X");
+    assertEquals(stringFormatter.addLeftPadding("X", 5), "    X");
+  }
+
+  @Test
+  void addRightPadding_ShouldAddRightPadding() {
+    assertEquals(stringFormatter.addRightPadding("X", 1), "X");
+    assertEquals(stringFormatter.addRightPadding("X", 3), "X  ");
+    assertEquals(stringFormatter.addRightPadding("X", 5), "X    ");
+  }
+}
