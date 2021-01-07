a1816e903476afaca8b5df629b645b53efb416ee
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2021-01-07 18:10:18 +0000

    Implement addBilateralPadding method in StringFormatter

--- src/main/java/uk/caputo/dotfilesmanager/StringFormatter.java
+++ src/main/java/uk/caputo/dotfilesmanager/StringFormatter.java
@@ -35,4 +35,23 @@ public class StringFormatter {
     String formatting = "%-" + length + "s";
     return String.format(formatting, content);
   }
+
+  /**
+   * Adds padding to both sides of a string. The padding is only added if the original length of the
+   * string is smaller than the given one, and only enough to match the given length. If the given
+   * length is an odd integer, the space in excess will be added to the right side of the string.
+   *
+   * @param content the string that has to be padded.
+   * @param length the length of the padded string.
+   * @return the padded string.
+   */
+  public String addBilateralPadding(String content, int length) {
+    length -= content.length();
+    if (length <= 0) {
+      throw new IllegalArgumentException("Content length is greater than requested length.");
+    }
+    int leftPadding = length / 2;
+    int rightPadding = (length / 2) + length % 2;
+    return " ".repeat(leftPadding) + content + " ".repeat(rightPadding);
+  }
 }
