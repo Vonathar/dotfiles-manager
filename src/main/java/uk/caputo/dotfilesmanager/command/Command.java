2c18c0cc3bb6e61123a11415a61503ffd536980e
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2021-01-10 10:22:41 +0000

    Implement Command class.

---
+++ src/main/java/uk/caputo/dotfilesmanager/command/Command.java
@@ -0,0 +1,46 @@
+package uk.caputo.dotfilesmanager.command;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import uk.caputo.dotfilesmanager.StringFormatter;
+
+/**
+ * This class represents a generic shell command, and must be implemented by each concrete
+ * implementation of a command. Whenever a new command extends this class, a reference to it is
+ * automatically added in the static list 'implementers'; this is done in the constructor, to allow
+ * easier access to all existing commands.
+ *
+ * @author Gianmarco Caputo
+ */
+public abstract class Command {
+
+  private static final List<Command> implementers = new ArrayList<>();
+  private final String command;
+  private final String description;
+  @Autowired private StringFormatter stringFormatter;
+
+  public Command(String command, String description) {
+    this.command = command;
+    this.description = description;
+    implementers.add(this);
+  }
+
+  /**
+   * Returns the list of all classes that extend the Command class.
+   *
+   * @return the list of Command implementers.
+   */
+  public static List<Command> getImplementers() {
+    return implementers;
+  }
+
+  /**
+   * Returns the formatted command information, ready to be printed to the shell.
+   *
+   * @return the formatted command information.
+   */
+  public String getInfo() {
+    return stringFormatter.separateSides(command, description, 15);
+  }
+}
