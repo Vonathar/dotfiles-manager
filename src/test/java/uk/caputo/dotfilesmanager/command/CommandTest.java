3335e394767e666e2c5f2bed53e5d166da7721b6
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2021-01-10 17:47:24 +0000

    Add tests for getDetailedInfo method in Command class.

--- src/test/java/uk/caputo/dotfilesmanager/command/CommandTest.java
+++ src/test/java/uk/caputo/dotfilesmanager/command/CommandTest.java
@@ -2,6 +2,7 @@ package uk.caputo.dotfilesmanager.command;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.LinkedHashMap;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,12 +28,42 @@ class CommandTest {
     assertEquals(expectedResult, actualResult);
   }
 
+  @Test
+  void getDetailedInfo_ShouldPrintFormattedCommandWithUsageExamplesAndOptions() {
+    Command command = new TestCommand();
+    ReflectionTestUtils.setField(command, "stringFormatter", stringFormatter);
+    String actualResult = command.getDetailedInfo();
+    String expectedResult =
+        "Test.\n"
+            + "\n"
+            + "Usage:\n"
+            + "  test -f <x>\n"
+            + "  test -b <y>\n\n"
+            + "Options:\n"
+            + "  -f --foo     description of foo.\n"
+            + "  -b --bar     description of bar.\n\n";
+    assertEquals(expectedResult, actualResult);
+  }
+
   static class TestCommand extends Command {
     @Override
     public String getDescription() {
       return "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec v"
           + "ulputate luctus purus, nec cursus enim euismod in. Vivamus in bib"
           + "endum ex, nec consequat libero. Proin molestie turpis nec aliquam.";
+    }
+
+    @Override
+    public LinkedHashMap<String, String> getOptions() {
+      LinkedHashMap<String, String> map = new LinkedHashMap<>();
+      map.put("-f --foo", "description of foo.");
+      map.put("-b --bar", "description of bar.");
+      return map;
+    }
+
+    @Override
+    public String[] getUsageExamples() {
+      return new String[] {"test -f <x>", "test -b <y>"};
     }
   }
 }
